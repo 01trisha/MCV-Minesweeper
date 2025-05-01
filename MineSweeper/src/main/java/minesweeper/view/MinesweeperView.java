36dbdb423983a21dada3e2287d86b05196ba25e2
01trisha <daniltrishkin5@gmail.com>
2025-05-02 02:34:55 +0700

    переделал вью и контроллер, теперь вью вызывает контроллер а не наоборот

--- MineSweeper/src/main/java/minesweeper/view/MinesweeperView.java
+++ MineSweeper/src/main/java/minesweeper/view/MinesweeperView.java
@@ -1,15 +1,16 @@
 package minesweeper.view;
 
 import minesweeper.controller.MinesweeperController;
-import minesweeper.model.MinesweeperModel;
 import minesweeper.observ.Observer;
 
 public interface MinesweeperView extends Observer {
+    void setController(MinesweeperController controller);
+    void start();
     void displayGame();
-    void printStartMessage();
-    void printEndMessage();
-    void printLostMessage();
-    void printField();
-    void showMessage(String text);
-    void printDifficult();
+//    void printStartMessage();
+//    void printEndMessage();
+//    void printLostMessage();
+//    void printField();
+//    void showMessage(String text);
+//    void printDifficult();
 }
